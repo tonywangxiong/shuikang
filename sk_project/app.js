9f42b5f0e58336154087b1e699ce9864ec03926a
‘e <35877320@qq.com>
2018-04-10 13:50:56 +0800

    add sk

---
+++ sk_project/app.js
@@ -0,0 +1,17 @@
+var express=require('express');
+var app=express();
+app.listen(9999);
+app.configure(function () {
+    app.use(app.router);
+    app.use(express.static(__dirname+"/public"));
+    app.use(express.favicon(__dirname+"/public/favicon.ico"));
+    app.use(express.errorHandler());
+    app.use(express.logger("dev"));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+
+});
+app.post('/index.do',function (req,res) {
+    console.log('111111');
+    // var myData=
+});
